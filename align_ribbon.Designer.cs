abff26b7381bd181d15a77f0e45ad1b65982d7a3
Juzhan Xu <juzhan.xu@gmail.com>
2022-03-20 20:39:09 +0800

    添加项目文件。

---
+++ align_ribbon.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace AlignTool
+{
+    partial class align_ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public align_ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.tool_group = this.Factory.CreateRibbonGroup();
+            this.align_menu = this.Factory.CreateRibbonMenu();
+            this.left = this.Factory.CreateRibbonButton();
+            this.horizontal = this.Factory.CreateRibbonButton();
+            this.right = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.top = this.Factory.CreateRibbonButton();
+            this.vertical = this.Factory.CreateRibbonButton();
+            this.bottom = this.Factory.CreateRibbonButton();
+            this.separator2 = this.Factory.CreateRibbonSeparator();
+            this.hori_dist = this.Factory.CreateRibbonButton();
+            this.vert_dist = this.Factory.CreateRibbonButton();
+            this.separator3 = this.Factory.CreateRibbonSeparator();
+            this.hori_group = this.Factory.CreateRibbonButton();
+            this.vert_group = this.Factory.CreateRibbonButton();
+            this.separator4 = this.Factory.CreateRibbonSeparator();
+            this.to_slide = this.Factory.CreateRibbonButton();
+            this.to_shape = this.Factory.CreateRibbonButton();
+            this.copy = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.tool_group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabHome";
+            this.tab1.Groups.Add(this.tool_group);
+            this.tab1.Label = "TabHome";
+            this.tab1.Name = "tab1";
+            // 
+            // tool_group
+            // 
+            this.tool_group.Items.Add(this.align_menu);
+            this.tool_group.Items.Add(this.copy);
+            this.tool_group.Label = "Tools";
+            this.tool_group.Name = "tool_group";
+            // 
+            // align_menu
+            // 
+            this.align_menu.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.align_menu.Description = "在选择的形状集合中，以最后一个形状为基准对齐";
+            this.align_menu.Image = global::AlignTool.Properties.Resources.align;
+            this.align_menu.Items.Add(this.left);
+            this.align_menu.Items.Add(this.horizontal);
+            this.align_menu.Items.Add(this.right);
+            this.align_menu.Items.Add(this.separator1);
+            this.align_menu.Items.Add(this.top);
+            this.align_menu.Items.Add(this.vertical);
+            this.align_menu.Items.Add(this.bottom);
+            this.align_menu.Items.Add(this.separator2);
+            this.align_menu.Items.Add(this.hori_dist);
+            this.align_menu.Items.Add(this.vert_dist);
+            this.align_menu.Items.Add(this.separator3);
+            this.align_menu.Items.Add(this.hori_group);
+            this.align_menu.Items.Add(this.vert_group);
+            this.align_menu.Items.Add(this.separator4);
+            this.align_menu.Items.Add(this.to_slide);
+            this.align_menu.Items.Add(this.to_shape);
+            this.align_menu.Label = "对齐";
+            this.align_menu.Name = "align_menu";
+            this.align_menu.ShowImage = true;
+            this.align_menu.SuperTip = "在选择的形状集合中，以最后一个形状为基准对齐";
+            // 
+            // left
+            // 
+            this.left.Image = global::AlignTool.Properties.Resources.left;
+            this.left.Label = "左对齐";
+            this.left.Name = "left";
+            this.left.ShowImage = true;
+            this.left.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.left_Click);
+            // 
+            // horizontal
+            // 
+            this.horizontal.Image = global::AlignTool.Properties.Resources.hori;
+            this.horizontal.Label = "横向居中";
+            this.horizontal.Name = "horizontal";
+            this.horizontal.ShowImage = true;
+            this.horizontal.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.horizontal_Click);
+            // 
+            // right
+            // 
+            this.right.Image = global::AlignTool.Properties.Resources.right;
+            this.right.Label = "右对齐";
+            this.right.Name = "right";
+            this.right.ShowImage = true;
+            this.right.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.right_Click);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // top
+            // 
+            this.top.Image = global::AlignTool.Properties.Resources.top;
+            this.top.Label = "顶对齐";
+            this.top.Name = "top";
+            this.top.ShowImage = true;
+            this.top.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.top_Click);
+            // 
+            // vertical
+            // 
+            this.vertical.Image = global::AlignTool.Properties.Resources.vert;
+            this.vertical.Label = "纵向对齐";
+            this.vertical.Name = "vertical";
+            this.vertical.ShowImage = true;
+            this.vertical.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.vertical_Click);
+            // 
+            // bottom
+            // 
+            this.bottom.Image = global::AlignTool.Properties.Resources.bottom;
+            this.bottom.Label = "底对齐";
+            this.bottom.Name = "bottom";
+            this.bottom.ShowImage = true;
+            this.bottom.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.bottom_Click);
+            // 
+            // separator2
+            // 
+            this.separator2.Name = "separator2";
+            // 
+            // hori_dist
+            // 
+            this.hori_dist.Image = global::AlignTool.Properties.Resources.hori_dist;
+            this.hori_dist.Label = "横向等距";
+            this.hori_dist.Name = "hori_dist";
+            this.hori_dist.ShowImage = true;
+            this.hori_dist.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.hori_dist_Click);
+            // 
+            // vert_dist
+            // 
+            this.vert_dist.Image = global::AlignTool.Properties.Resources.vert_dist;
+            this.vert_dist.Label = "纵向等距";
+            this.vert_dist.Name = "vert_dist";
+            this.vert_dist.ShowImage = true;
+            this.vert_dist.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.vert_dist_Click);
+            // 
+            // separator3
+            // 
+            this.separator3.Name = "separator3";
+            // 
+            // hori_group
+            // 
+            this.hori_group.Image = global::AlignTool.Properties.Resources.hori_group;
+            this.hori_group.Label = "横向居中（集合）";
+            this.hori_group.Name = "hori_group";
+            this.hori_group.ShowImage = true;
+            this.hori_group.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.hori_group_Click);
+            // 
+            // vert_group
+            // 
+            this.vert_group.Image = global::AlignTool.Properties.Resources.vert_group;
+            this.vert_group.Label = "纵向居中（集合）";
+            this.vert_group.Name = "vert_group";
+            this.vert_group.ShowImage = true;
+            this.vert_group.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.vert_group_Click);
+            // 
+            // separator4
+            // 
+            this.separator4.Name = "separator4";
+            // 
+            // to_slide
+            // 
+            this.to_slide.Label = "与幻灯片对齐";
+            this.to_slide.Name = "to_slide";
+            this.to_slide.ShowImage = true;
+            this.to_slide.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.to_slide_Click);
+            // 
+            // to_shape
+            // 
+            this.to_shape.Label = "与形状对齐";
+            this.to_shape.Name = "to_shape";
+            this.to_shape.ShowImage = true;
+            this.to_shape.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.to_shape_Click);
+            // 
+            // copy
+            // 
+            this.copy.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.copy.Description = "复制形状，并且和被复制形状位置一样";
+            this.copy.Image = global::AlignTool.Properties.Resources.copy;
+            this.copy.Label = "原位复制";
+            this.copy.Name = "copy";
+            this.copy.ShowImage = true;
+            this.copy.SuperTip = "复制新的形状，并且和被复制形状位置一样";
+            this.copy.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.copy_Click);
+            // 
+            // align_ribbon
+            // 
+            this.Name = "align_ribbon";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.align_ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.tool_group.ResumeLayout(false);
+            this.tool_group.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup tool_group;
+        internal Microsoft.Office.Tools.Ribbon.RibbonMenu align_menu;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton left;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton horizontal;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton right;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton top;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton vertical;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton bottom;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton hori_dist;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton vert_dist;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton to_shape;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton to_slide;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton copy;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton hori_group;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton vert_group;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal align_ribbon align_ribbon
+        {
+            get { return this.GetRibbon<align_ribbon>(); }
+        }
+    }
+}
---
+++ align_ribbon.cs
@@ -0,0 +1,373 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace AlignTool
+{
+    public partial class align_ribbon
+    {
+        bool is_to_shape = true;
+
+        private void align_ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            update_enable();
+        }
+
+        private void update_enable()
+        {
+            to_shape.Enabled = !is_to_shape;
+            to_slide.Enabled = is_to_shape;
+        }
+
+        private void left_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("left");
+        }
+
+        private void horizontal_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("horizontal");
+
+        }
+
+        private void right_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("right");
+
+        }
+
+        private void top_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("top");
+        }
+
+        private void vertical_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("vertical");
+        }
+
+        private void bottom_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("bottom");
+
+        }
+
+        private void hori_dist_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("hori_dist");
+
+        }
+
+        private void vert_dist_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("vert_dist");
+
+        }
+
+        private void hori_group_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("hori_group");
+
+        }
+        private void vert_group_Click(object sender, RibbonControlEventArgs e)
+        {
+            align_shapes("vert_group");
+        }
+
+        private void to_slide_Click(object sender, RibbonControlEventArgs e)
+        {
+            is_to_shape = false;
+            update_enable();
+        }
+
+        private void to_shape_Click(object sender, RibbonControlEventArgs e)
+        {
+            is_to_shape = true;
+            update_enable();
+        }
+
+        private void copy_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                if (Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange.Count > 0)
+                {
+                    var shape = Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange;
+
+                    int count = shape.Count;
+
+
+                    var shapeRange = Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange;
+
+                    foreach (PowerPoint.Shape shp in shapeRange)
+                    {
+                        shp.Copy();
+                        var new_shp = Globals.ThisAddIn.Application.ActiveWindow.View.Slide.Shapes.Paste();
+
+                        new_shp.Top = shp.Top;
+                        new_shp.Left = shp.Left;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        private void align_shapes(string align_type)
+        {
+            try
+            {
+                if (Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange.Count > 0)
+                {
+                    var shape = Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange;
+                    
+                    int count = shape.Count;
+
+                    bool need_slide = false;
+
+                    if (count == 1)
+                    {
+                        if (align_type == "hori_dist" || align_type == "vert_dist") { return; }
+                    }
+
+                    if (!this.is_to_shape || count == 1) { need_slide = true; }
+
+                    if (need_slide) { count += 1; }
+
+                    int[] index = new int[count];
+                    for (int j = 0; j < count; j++)
+                    {
+                        index[j] = j;
+                    }
+
+                    float[] centers_x = new float[count];
+                    float[] centers_y = new float[count];
+                    float[,] sizes = new float[count, 2];
+                    float[,] corners = new float[count, 2];
+
+                    float left_top_x = 99999;
+                    float left_top_y = 99999;
+                    float right_bottom_x = 0;
+                    float right_bottom_y = 0;
+
+                    var shapeRange = Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange;
+                    int i = 0;
+                    foreach (PowerPoint.Shape shp in shapeRange)
+                    {
+
+                        float w = shp.Width;
+                        float h = shp.Height;
+                        float t = shp.Top;
+                        float l = shp.Left;
+
+                        centers_x[i] = l + w / 2;
+                        centers_y[i] = t + h / 2;
+
+                        sizes[i, 0] = w;
+                        sizes[i, 1] = h;
+
+                        corners[i, 0] = l;
+                        corners[i, 1] = t;
+
+                        float r = l + w;
+                        float b = t + h;
+
+                        if (left_top_x > l) left_top_x = l;
+                        if (left_top_y > t) left_top_y = t;
+                        if (right_bottom_x < r) right_bottom_x = r;
+                        if (right_bottom_y < b) right_bottom_y = b;
+
+                        i++;
+                    }
+
+                    i = count - 1;
+                    if (need_slide)
+                    {
+                        float w = Globals.ThisAddIn.Application.ActivePresentation.PageSetup.SlideWidth;
+                        float h = Globals.ThisAddIn.Application.ActivePresentation.PageSetup.SlideHeight;
+                        float l = 0;
+                        float t = 0;
+
+                        centers_x[i] = l + w / 2;
+                        centers_y[i] = t + h / 2;
+
+                        sizes[i, 0] = w;
+                        sizes[i, 1] = h;
+
+                        corners[i, 0] = l;
+                        corners[i, 1] = t;
+                    }
+
+                    // 开始对齐
+
+                    if (align_type == "hori_dist" )
+                    {
+                        Array.Sort(centers_x, index);
+
+                        int left_id = index[0];
+                        int right_id = index[count-1];
+
+                        float left_w = sizes[left_id, 0];
+                        float right_w = sizes[right_id, 0];
+
+                        float between_gap = (centers_x[count-1] - right_w / 2) - (centers_x[0] + left_w / 2) ;
+                        float total_len = 0;
+                        foreach (var id in index)
+                        {
+                            if (id == left_id || id == right_id) continue;
+                            total_len += sizes[id, 0];
+                        }
+                        between_gap = between_gap - total_len;
+
+                        between_gap /= count - 1;
+
+                        float pre_right = centers_x[0] + left_w / 2;
+
+                        foreach (var id in index)
+                        {
+                            if (id == left_id || id == right_id) continue;
+
+                            int k = 0;
+                            foreach (PowerPoint.Shape shp in shapeRange)
+                            {
+
+                                if (id == k) { 
+                                    float shp_w = shp.Width;
+
+                                    pre_right += between_gap;
+                                    shp.Left = pre_right;
+
+                                    pre_right += shp_w;
+                                }
+                                k++;
+                            }
+                        }
+
+                    }
+                    else if (align_type == "vert_dist")
+                    {
+                        Array.Sort(centers_y, index);
+
+                        int top_id = index[0];
+                        int bottom_id = index[count - 1];
+
+                        float top_h = sizes[top_id, 1];
+                        float bottom_h = sizes[bottom_id, 1];
+
+                        float between_gap = (centers_y[count - 1] - bottom_h / 2) - (centers_y[0] + top_h / 2);
+                        float total_len = 0;
+                        foreach (var id in index)
+                        {
+                            if (id == top_id || id == bottom_id) continue;
+                            total_len += sizes[id, 1];
+                        }
+                        between_gap = between_gap - total_len;
+
+                        between_gap /= count - 1;
+
+                        float pre_botttom = centers_y[0] + top_h / 2;
+
+                        foreach (var id in index)
+                        {
+                            if (id == top_id || id == bottom_id) continue;
+
+                            int k = 0;
+                            foreach (PowerPoint.Shape shp in shapeRange)
+                            {
+
+                                if (id == k)
+                                {
+                                    float shp_h = shp.Height;
+
+                                    pre_botttom += between_gap;
+                                    shp.Top = pre_botttom;
+
+                                    pre_botttom += shp_h;
+                                }
+                                k++;
+                            }
+                        }
+                    }
+                    else if (align_type == "hori_group")
+                    {
+                        float group_center_y = (left_top_y + right_bottom_y) / 2;
+
+                        float slide_h = Globals.ThisAddIn.Application.ActivePresentation.PageSetup.SlideHeight;
+
+                        float slide_center_y = slide_h / 2;
+
+                        float slide_y_offset = slide_center_y - group_center_y;
+
+                        foreach (PowerPoint.Shape shp in shapeRange)
+                        {
+                            shp.Top += slide_y_offset;
+                        }
+                    }
+                    else if (align_type == "vert_group")
+                    {
+                        float group_center_x = (left_top_x + right_bottom_x) / 2;
+                        
+                        float slide_w = Globals.ThisAddIn.Application.ActivePresentation.PageSetup.SlideWidth;
+                        
+                        float slide_center_x = slide_w / 2;
+
+                        float slide_x_offset = slide_center_x - group_center_x;
+
+                        foreach (PowerPoint.Shape shp in shapeRange)
+                        {
+                            shp.Left += slide_x_offset;
+                        }
+                    }
+                    else
+                    {
+                        i = 0;
+                        foreach (PowerPoint.Shape shp in shapeRange)
+                        {
+                            switch (align_type)
+                            {
+                                case "horizontal":
+                                    shp.Top = centers_y[count - 1] - sizes[i, 1] / 2;
+                                    break;
+                                case "vertical":
+                                    shp.Left = centers_x[count - 1] - sizes[i, 0] / 2;
+                                    break;
+
+                                case "left":
+                                    shp.Left = corners[count - 1, 0];
+                                    break;
+                                case "right":
+                                    shp.Left = (corners[count - 1, 0] + sizes[count - 1, 0] - sizes[i, 0]);
+                                    break;
+
+                                case "top":
+                                    shp.Top = corners[count - 1, 1];
+                                    break;
+                                case "bottom":
+                                    shp.Top = (corners[count - 1, 1] + sizes[count-1, 1] - sizes[i, 1] );
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                            i++;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
